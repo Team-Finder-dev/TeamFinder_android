fd9a752bb9fc2661014eff0490e45195cc714db2
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-20 21:10:33 +0300

    Добавляет фрагменты регистрации и поиска (для кейса со входом без регистрации) Фрагменты добавлены в граф навигации

---
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/registration/RegistrationViewModel.kt
@@ -0,0 +1,7 @@
+package com.teamfinder.teamfinder.presentation.registration
+
+import androidx.lifecycle.ViewModel
+
+
+class RegistrationViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/searchscreen/SearchViewModel.kt
@@ -0,0 +1,6 @@
+package com.teamfinder.teamfinder.presentation.searchscreen
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel : ViewModel() {
+}
