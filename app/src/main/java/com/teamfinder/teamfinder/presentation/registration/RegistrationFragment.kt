5d2f8ccaec7562450dd4a4d6d326c99c10d0fcb4
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-20 22:12:24 +0300

    Добавляет фрагменты контакты и поиска команд, игроков Добавляет иконки для BottomNavigationView Добавляет добавляет фрагменты на nav_graph.xml Добавляет action для перехода из фрагмента регистрации во фрагмент поиска

--- app/src/main/java/com/teamfinder/teamfinder/presentation/registration/RegistrationFragment.kt
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/registration/RegistrationFragment.kt
@@ -1,6 +1,9 @@
 package com.teamfinder.teamfinder.presentation.registration
 
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.teamfinder.teamfinder.R
 import com.teamfinder.teamfinder.base.BaseFragment
 import com.teamfinder.teamfinder.databinding.FragmentRegistrationBinding
 
@@ -10,5 +13,19 @@ class RegistrationFragment :
 
     override fun subscribe() {
         super.subscribe()
+        clickListeners()
+    }
+
+    private fun clickListeners() = with(binding) {
+        val listener = onClickListener()
+        btnToSearchScreen.setOnClickListener(listener)
+    }
+
+    private fun onClickListener() = View.OnClickListener {
+        with(binding) {
+            when (it) {
+                btnToSearchScreen -> findNavController().navigate(R.id.action_registrationFragment_to_searchFragment)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/teamssearch/TeamsSearchFragment.kt
@@ -0,0 +1,7 @@
+package com.teamfinder.teamfinder.presentation.teamssearch
+
+import androidx.fragment.app.Fragment
+
+class TeamsSearchFragment : Fragment() {
+
+}
