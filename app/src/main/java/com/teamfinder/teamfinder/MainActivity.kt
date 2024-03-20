dc18b1e2e074746c6c857caa8a259cd459f80840
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-20 20:45:22 +0300

    Подключает binding к Activity

--- app/src/main/java/com/teamfinder/teamfinder/MainActivity.kt
+++ app/src/main/java/com/teamfinder/teamfinder/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.teamfinder.teamfinder
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.teamfinder.teamfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
     }
 }
