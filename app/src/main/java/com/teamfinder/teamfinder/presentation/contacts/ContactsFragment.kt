6f10d1a82357966192bd3a610e8591fb996542c7
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-20 22:12:24 +0300

    Добавляет фрагменты контакты и поиска команд, игроков Добавляет иконки для BottomNavigationView Добавляет добавляет фрагменты на nav_graph.xml Добавляет action для перехода из фрагмента регистрации во фрагмент поиска

---
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/contacts/ContactsFragment.kt
@@ -0,0 +1,11 @@
+package com.teamfinder.teamfinder.presentation.contacts
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.teamfinder.teamfinder.R
+
+class ContactsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/playerssearch/PlayersSearchFragment.kt
@@ -0,0 +1,11 @@
+package com.teamfinder.teamfinder.presentation.playerssearch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.teamfinder.teamfinder.R
+
+class PlayersSearchFragment : Fragment() {
+}
