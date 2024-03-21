d8ccce352a1c91feb550357346781588f7a4cace
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-21 13:02:06 +0300

    корректирует инициализацию navController в SearchFragment

--- app/src/main/java/com/teamfinder/teamfinder/presentation/searchscreen/SearchFragment.kt
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/searchscreen/SearchFragment.kt
@@ -3,6 +3,7 @@ package com.teamfinder.teamfinder.presentation.searchscreen
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.teamfinder.teamfinder.R
 import com.teamfinder.teamfinder.base.BaseFragment
@@ -18,7 +19,7 @@ class SearchFragment :
     }
 
     private fun initBottomNavigationBar() {
-        navController = requireActivity().findNavController(R.id.container_view)
+        navController = (childFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment).navController
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
