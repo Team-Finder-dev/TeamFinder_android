da540af042ee603c6733b109038bf2627e00a955
Dmitry Kryushnikov <civ02@mail.ru>
2024-03-22 22:20:58 +0500

    тест коммит

--- app/src/main/java/com/teamfinder/teamfinder/MainActivity.kt
+++ app/src/main/java/com/teamfinder/teamfinder/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val test = "dfg df gfd g fd g fdg fd g fg fdg fd g df gdf g f g fdg df g fdg fd g fd g fdg fd g fdg fd gf dg df gfd g df  fdg fd g fdg df gdf g fd gfd g fd gdf "
+
+
+
+        var GROMvSHTANAH2sk = listOf("gd","sdfgs","sdfg","fs").subList(1,2).last().length.also { Unit   }
     }
 }
