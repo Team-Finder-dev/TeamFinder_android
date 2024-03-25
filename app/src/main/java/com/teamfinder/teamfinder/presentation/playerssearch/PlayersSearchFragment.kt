dacf8827fcede42922951775d17fa24008e5d3d1
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-25 17:33:50 +0300

    Добавляет метоод для инициализации спиннера

--- app/src/main/java/com/teamfinder/teamfinder/presentation/playerssearch/PlayersSearchFragment.kt
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/playerssearch/PlayersSearchFragment.kt
@@ -1,10 +1,51 @@
 package com.teamfinder.teamfinder.presentation.playerssearch
 
-import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
+import com.teamfinder.teamfinder.R
 import com.teamfinder.teamfinder.base.BaseFragment
 import com.teamfinder.teamfinder.databinding.FragmentPlayersSearchBinding
+import com.teamfinder.teamfinder.presentation.playerssearch.adapters.CustomArrayAdapter
 
-class PlayersSearchFragment : BaseFragment<FragmentPlayersSearchBinding, PlayersSearchViewModel>(FragmentPlayersSearchBinding::inflate) {
+class PlayersSearchFragment :
+    BaseFragment<FragmentPlayersSearchBinding, PlayersSearchViewModel>(FragmentPlayersSearchBinding::inflate) {
     override val viewModel by viewModels<PlayersSearchViewModel>()
+
+
+    override fun initViews() {
+        super.initViews()
+        initSpinners()
+    }
+
+    private fun initSpinners() = with(binding) {
+        initSpinner(resources.getStringArray(R.array.sport_type), spinnerSportType)
+    }
+
+    private fun initSpinner(stringArray: Array<String>, spinner: Spinner) {
+        val adapter =
+            CustomArrayAdapter(requireContext(), R.layout.item_dropdown_spinner, stringArray)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position > 0) {
+                    viewModel.filterData()
+                } else {
+                    viewModel.showData()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) = Unit
+        }
+    }
+
+    private fun searchInputListener() = Unit
+
 }
--- app/src/main/java/com/teamfinder/teamfinder/presentation/playerssearch/PlayersSearchViewModel.kt
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/playerssearch/PlayersSearchViewModel.kt
@@ -3,4 +3,7 @@ package com.teamfinder.teamfinder.presentation.playerssearch
 import com.teamfinder.teamfinder.base.BaseViewModel
 
 class PlayersSearchViewModel: BaseViewModel() {
+
+    fun showData() = Unit
+    fun filterData() = Unit
 }
