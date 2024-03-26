450583313c237e07ba479605a059bf9f50057a62
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-26 18:27:15 +0300

    Удаляет не используемые классы Удаляет адаптер для спиннера

--- app/src/main/java/com/teamfinder/teamfinder/presentation/playerssearch/PlayersSearchViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.teamfinder.teamfinder.presentation.playerssearch
-
-import com.teamfinder.teamfinder.base.BaseViewModel
-
-class PlayersSearchViewModel: BaseViewModel() {
-
-    fun showData() = Unit
-    fun filterData() = Unit
-}
