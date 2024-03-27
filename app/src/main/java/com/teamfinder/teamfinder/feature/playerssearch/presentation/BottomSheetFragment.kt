d6fa662691d3842f8dd221634c4b30e2d57ba90c
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-03-28 00:37:49 +0300

    Подключает binding в BottomSheetFragment

--- app/src/main/java/com/teamfinder/teamfinder/feature/playerssearch/presentation/BottomSheetFragment.kt
+++ app/src/main/java/com/teamfinder/teamfinder/feature/playerssearch/presentation/BottomSheetFragment.kt
@@ -1,22 +1,44 @@
 package com.teamfinder.teamfinder.feature.playerssearch.presentation
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.teamfinder.teamfinder.R
+import com.teamfinder.teamfinder.databinding.BottomSheetSingleSelectorBinding
 import com.teamfinder.teamfinder.feature.playerssearch.presentation.adapters.CharactersAdapter
 
 class BottomSheetFragment(layoutId: Int, private val stringsArr: Array<CharSequence>) :
     BottomSheetDialogFragment(layoutId) {
+    private var _binding: BottomSheetSingleSelectorBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BottomSheetSingleSelectorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
 
-        view.findViewById<TextView>(R.id.tv_title).text = stringsArr[0]
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initViews() = with(binding) {
+        tvTitle.text = stringsArr[0]
         val subArray = stringsArr.sliceArray(1..stringsArr.lastIndex)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = CharactersAdapter(subArray.map { it.toString() })
     }
