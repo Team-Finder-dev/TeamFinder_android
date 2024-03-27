4ca60cadcfe7adb8569c7ffb979677abeb4edf20
Max <bespalov.m.9@ya.ru>
2024-03-27 21:29:09 +0300

    padding between icon and title was removed in TeamDetailsFragment tablayout

--- app/src/main/java/com/teamfinder/teamfinder/presentation/teamdetails/TeamDetailsFragment.kt
+++ app/src/main/java/com/teamfinder/teamfinder/presentation/teamdetails/TeamDetailsFragment.kt
@@ -2,8 +2,10 @@ package com.teamfinder.teamfinder.presentation.teamdetails
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamfinder.teamfinder.R
 import com.teamfinder.teamfinder.base.BaseFragment
@@ -13,7 +15,7 @@ class TeamDetailsFragment :
     BaseFragment<FragmentTeamDetailsBinding, TeamDetailsViewModel>(FragmentTeamDetailsBinding::inflate) {
 
     override val viewModel by viewModels<TeamDetailsViewModel>()
-    private  var tabMediator: TabLayoutMediator? = null
+    private var tabMediator: TabLayoutMediator? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,15 +38,43 @@ class TeamDetailsFragment :
             when (position) {
                 0 -> {
                     tab.text = getString(R.string.basic)
-                    tab.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_teamdetails_basic_tab)
+                    tab.icon = AppCompatResources.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_teamdetails_basic_tab
+                    )
                 }
+
                 1 -> {
                     tab.text = getString(R.string.info)
-                    tab.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_teamdetails_info_tab)
+                    tab.icon = AppCompatResources.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_teamdetails_info_tab
+                    )
                 }
             }
         }
         tabMediator?.attach()
+        removeTabsIconMargins()
+        binding.teamDetailsTabLayout.addOnTabSelectedListener(object :
+            TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                removeTabsIconMargins()
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) { }
+
+            override fun onTabReselected(p0: TabLayout.Tab?) { }
+
+        })
+    }
+
+    private fun removeTabsIconMargins() {
+        for (i in 0..binding.teamDetailsTabLayout.tabCount) {
+            val params =
+                binding.teamDetailsTabLayout.getTabAt(i)?.view?.getChildAt(0)?.layoutParams as LinearLayout.LayoutParams?
+            params?.bottomMargin = 0
+            binding.teamDetailsTabLayout.getTabAt(i)?.view?.getChildAt(0)?.layoutParams = params
+        }
     }
 
 }
