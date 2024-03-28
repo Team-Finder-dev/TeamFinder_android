df82d9e58f862bff16cc3be9bf1bba573870980e
Max <bespalov.m.9@yandex.ru>
2024-03-28 08:51:02 +0300

    Changing color of status bar icons was added in mainActivity

--- app/src/main/java/com/teamfinder/teamfinder/MainActivity.kt
+++ app/src/main/java/com/teamfinder/teamfinder/MainActivity.kt
@@ -3,6 +3,8 @@ package com.teamfinder.teamfinder
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.fragment.NavHostFragment
 import com.teamfinder.teamfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Change color of status bar icons
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.registrationFragment -> setLightStatusBarIconsColor()
+                else -> removeLightStatusBarIconsColor()
+            }
+        }
+    }
+
+    private fun setLightStatusBarIconsColor() {
+        WindowInsetsControllerCompat(window, binding.root).isAppearanceLightStatusBars = true
+    }
+
+    private fun removeLightStatusBarIconsColor() {
+        WindowInsetsControllerCompat(window, binding.root).isAppearanceLightStatusBars = false
     }
 }
